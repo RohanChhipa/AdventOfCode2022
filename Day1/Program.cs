1db2cf59e5a7a39f2a6574dd6b268fa449d23ded
Rohan Chhipa <rohan.chhipa@entelect.co.za>
2022-12-01 15:05:36 +0200

    Add scaffolding and Day 1 solution

---
+++ Day1/Program.cs
@@ -0,0 +1,5 @@
+﻿var calories = File.ReadAllText("input.txt")
+    .Split("\n\n")
+    .Select(s => s.Split("\n").Select(int.Parse).Sum());
+
+Console.WriteLine($"{calories.Max()}, {calories.OrderByDescending(i => i).Take(3).Sum()}");
