5a2fffe90a2171ad61c61a1daeb5ae0baa2726da
Rohan Chhipa <rohan.chhipa@entelect.co.za>
2022-12-04 06:15:14 +0200

    Add Day 4

---
+++ Day4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+
+var stopwatch = new Stopwatch();
+stopwatch.Start();
+
+var lines = File.ReadAllLines("input.txt");
+
+var taskOne = 0;
+var taskTwo = 0;
+foreach (var line in lines)
+{
+    var input = line.Split(",")
+        .Select(s => s.Split("-")
+            .Select(int.Parse)
+            .ToArray())
+        .ToArray();
+
+    if (input[0][0] <= input[1][0] && input[0][1] >= input[1][1]
+        || input[1][0] <= input[0][0] && input[1][1] >= input[0][1])
+        taskOne++;
+
+    if (input[0][0] <= input[1][0] && input[0][1] >= input[1][0]
+        || input[1][0] <= input[0][0] && input[1][1] >= input[0][0])
+        taskTwo++;
+}
+
+Console.WriteLine($"Task One: {taskOne} Task Two: {taskTwo} Time: {stopwatch.ElapsedMilliseconds}");
