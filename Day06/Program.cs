2aebd946fd1340b896228197533fd6972645f246
Rohan Chhipa <rohan.chhipa@entelect.co.za>
2022-12-06 07:19:39 +0200

    Add Day 6

---
+++ Day06/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+
+var stopwatch = new Stopwatch();
+stopwatch.Start();
+
+var line = File.ReadAllText("input.txt");
+var find = (int markerLength) => Enumerable.Range(0, line.Length - markerLength - 1)
+    .Select(i =>
+    {
+        var markerFound = line.Substring(i, markerLength)
+            .GroupBy(c => c)
+            .All(chars => chars.Count() == 1);
+
+        return (markerFound, i + markerLength);
+    })
+    .First(tuple => tuple.markerFound);
+
+Console.WriteLine($"Task One: {find(4).Item2}");
+Console.WriteLine($"Task Two: {find(14).Item2}");
+Console.WriteLine($"Time: {stopwatch.ElapsedMilliseconds}");
